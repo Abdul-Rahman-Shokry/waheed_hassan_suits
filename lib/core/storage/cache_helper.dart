804a97440a5c095d32299d0f107f01afef4a9982
Abdul-Rahman-Shokry <120654285+Abdul-Rahman-Shokry@users.noreply.github.com>
2026-09-12 00:18:57 +0300

    refactor(cache_helper): store token & refresh_token in flutter_secure_storage

--- lib/core/storage/cache_helper.dart
+++ lib/core/storage/cache_helper.dart
@@ -1,12 +1,28 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../features/auth/models/user_model.dart';
 
 class CacheHelper {
   static late SharedPreferences _prefs;
+  static late FlutterSecureStorage _secureStorage;
+
+  static String? _token;
+  static String? _refreshToken;
+
+  static const String _tokenKey = "token";
+  static const String _refreshTokenKey = "refreshToken";
 
   static Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
+    _secureStorage = const FlutterSecureStorage(
+      aOptions: AndroidOptions(
+        resetOnError: true,
+      ),
+    );
+
+    _token = await _secureStorage.read(key: _tokenKey);
+    _refreshToken = await _secureStorage.read(key: _refreshTokenKey);
   }
 
   static void setIsNotFirstTime() {
@@ -18,48 +34,54 @@ class CacheHelper {
   }
 
   static bool get isLoggedIn {
-    return (_prefs.getString("token") ?? "").isNotEmpty;
+    return (_token ?? "").isNotEmpty;
   }
 
   static String? get token {
-    return _prefs.getString("token");
+    return _token;
+  }
+
+  static String? get refreshToken {
+    return _refreshToken;
   }
 
   static String get fullName {
     return _prefs.getString("fullName") ?? "fullName";
   }
 
-  // static String get profilePhotoUrl {
-  //   return _prefs.getString("profilePhotoUrl") ??
-  //       "https://img.icons8.com/color/1200/person-male.jpg";
-  // }
+  static String get email {
+    return _prefs.getString("email") ?? "";
+  }
 
   static Future<void> saveUserData({UserModel? model}) async {
     if (model == null) return;
 
-    _prefs.setString("email", model.email);
-    _prefs.setString("fullName", model.fullName);
-    _prefs.setString("token", model.token);
-    _prefs.setString("refreshToken", model.refreshToken);
-    _prefs.setString("message", model.message);
-    _prefs.setBool("isAuthenticated", model.isAuthenticated);
-    _prefs.setBool("requiresEmailVerification", model.requiresEmailVerification,);
-    _prefs.setString("userId", model.id);
+    _token = model.token;
+    _refreshToken = model.refreshToken;
+
+    await _secureStorage.write(key: _tokenKey, value: model.token);
+    await _secureStorage.write(key: _refreshTokenKey, value: model.refreshToken);
+
+    await _prefs.setString("email", model.email);
+    await _prefs.setString("fullName", model.fullName);
+    await _prefs.setString("message", model.message);
+    await _prefs.setBool("isAuthenticated", model.isAuthenticated);
+    await _prefs.setBool("requiresEmailVerification", model.requiresEmailVerification);
+    await _prefs.setString("userId", model.id);
   }
 
   static Future<void> clearSharedPrefs() async {
+    _token = null;
+    _refreshToken = null;
+
+    await _secureStorage.delete(key: _tokenKey);
+    await _secureStorage.delete(key: _refreshTokenKey);
+
     await _prefs.remove("email");
     await _prefs.remove("fullName");
-    await _prefs.remove("token");
-    await _prefs.remove("refreshToken");
     await _prefs.remove("message");
     await _prefs.remove("isAuthenticated");
     await _prefs.remove("requiresEmailVerification");
     await _prefs.remove("userId");
-    // don't remove `isFirstTime`
-  }
-
-  static String get email {
-    return _prefs.getString("email") ?? "";
   }
 }
